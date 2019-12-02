45078f70ee9b60c85af060c6d94cd38c1882df40
AnsenJiang <ansen2014@foxmail.com>
2019-12-02 13:21:17 +0800

    第七章节结束 拓展功能 但是感觉在默认使用get请求的问题上还有些缺失

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -1,24 +1,50 @@
 import axios from "../../src";
 
-axios({
-  url:'/extend/post',
-  method: 'post',
-  data:{
-    msg:'Hey'
-  }
-})
+// axios({
+//   url:'/extend/post',
+//   method: 'post',
+//   data:{
+//     msg:'Hey'
+//   }
+// })
 
-axios.request({
-  url:'/extend/post',
-  method:'post',
-  data:{
-    msg:'Hello request'
-  }
-})
+// axios.request({
+//   url:'/extend/post',
+//   method:'post',
+//   data:{
+//     msg:'Hello request'
+//   }
+// })
 
-axios.get('/extend/get')
-axios.options('/extend/options')
-axios.delete('/extend/delete')
-axios.head('/extend/head')
-axios.post('/extend/post', {msg:'post'})
-axios.put('/extend/put', {msg:'put'})
+// axios.get('/extend/get')
+// axios.options('/extend/options')
+// axios.delete('/extend/delete')
+// axios.head('/extend/head')
+// axios.post('/extend/post', {msg:'post'})
+// axios.put('/extend/put', {msg:'put'})
+
+interface ResponseData<T = any>{
+  code: number
+  result: T
+  message: string
+}
+
+interface User{
+  name: string
+  age: number
+}
+
+function getUser<T>(){
+  return axios<ResponseData<T>>({
+    url:'/extend/user'
+  })
+    .then(res=>res.data)
+    .catch(err => console.error(err))
+}
+async function test(){
+  const user = await getUser<User>()
+  if(user){
+    console.log(user.result)
+  }
+}
+test()
